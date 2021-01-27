298c815e593b7f7eb075d0b1aa1eccffe558cd2e
Robert <soriano@cbtseminary.org>
2021-01-27 09:25:27 -0600

    .

--- index.js
+++ index.js
@@ -32,5 +32,6 @@ app.get("*", (req, res) => {
 
 
 
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
+const port = 4000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
