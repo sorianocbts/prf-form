806aaf7d3ed50383d800b4d6ffc3d1ae71a27fdf
Roberto Soriano <soriano@cbtseminary.org>
2024-08-19 15:19:47 -0500

    .

--- prf-form/src/components/PRF.js
+++ prf-form/src/components/PRF.js
@@ -3,7 +3,19 @@ import Swal from 'sweetalert2'
 import Select from 'react-select'
 import useForm from "../helpers/useForm"
 import { testsData } from '../testsData'
+
 const PRF = () => {
+
+//added 08/19/2024
+const [language, setLanguage] = useState('en');
+
+// Function to toggle language
+const toggleLanguage = () => {
+  setLanguage(language === 'en' ? 'es' : 'en');
+};
+
+//
+
     const [titles, setTitles] = useState([])
     const [availTest, setAvailTest] = useState([])
     const [classCode, setClassCode] = useState('')
@@ -101,8 +113,79 @@ const PRF = () => {
 
     }
 
+    // return (
+    //     <div className="container">
+    //         <h2 style={{ color: '#0d6cbf' }}>Password Request Form</h2>
+    //         <div className="form-container">
+
+    //             <form id="mainForm">
+    //                 <div className="form-group w-100">
+    //                     <label htmlFor="studentFirst">First Name</label>
+    //                     <input required type="text" className="form-control" id="studentFirst" name="fname" value={user.fname} onChange={handleChange} />
+    //                 </div>
+    //                 <div className="form-group w-100">
+    //                     <label htmlFor="studentLast">Last Name</label>
+    //                     <input required type="text" className="form-control" id="studentLast" name="lname" value={user.lname} onChange={handleChange} />
+    //                 </div>
+    //                 <div className="form-group w-100">
+    //                     <label htmlFor="studentEmail">Student Email</label>
+    //                     <input required type="email" className="form-control" id="studentEmail" name="email" value={user.email} onChange={handleChange} />
+    //                     <small>student@example.com</small>
+    //                 </div>
+    //                 <div className="form-group w-100">
+    //                     <label htmlFor="proctorName">Proctor Name</label>
+    //                     <input required type="text" className="form-control" id="proctorName" name="pname" value={user.pname} onChange={handleChange} />
+    //                 </div>
+    //                 <div className="form-group w-100">
+    //                     <label htmlFor="proctorEmail">Proctor Email</label>
+    //                     <input required type="email" className="form-control" id="proctorEmail" name="pemail" value={user.pemail} onChange={handleChange} />
+    //                     <small>proctor@example.com</small>
+    //                 </div>
+    //                 <div className="form-group w-100">
+    //                     <label htmlFor="classCode">Class Code &amp; Title
+    //                         <span className="form-required">
+    //                             *
+    //                         </span></label>
+    //                     {/* <select required className="form-control" id="classCode" value={classCode} onChange={(e) => classCodeSelectChanged(e)}>
+    //                         <option value="none" defaultValue="" disabled="" hidden="">Select Class Code</option>
+    //                         {titles.map((x, idx) => (
+    //                             <option key={idx} value={x}>{x}</option>
+    //                         ))}
+    //                     </select> */}
+    //                     <Select options={titles.map((x, idx) => ({value:x, label:x}))} onChange={(e) => {classCodeSelectChanged(e.value)}} />
+    //                 </div>
+
+    //                 <div className="form-group w-100">
+    //                     <label htmlFor="testNumber">Quiz/Test number
+    //                         <span className="form-required">
+    //                             *
+    //                         </span></label>
+    //                     <select required className="form-control" id="testNumber" value={quizCode} onChange={(e) => selectedTest(e)}>
+    //                         <option value="none" defaultValue="" disabled="" hidden="">Select Quiz/Test</option>
+    //                         {availTest.map((x, idx) => (
+    //                             <option key={idx} value={x.testName}>{x.testName}</option>
+    //                         ))}
+
+    //                     </select>
+    //                 </div>
+    //                 <button type="submit" className="btn  btn-sm btn-block w-100 shadow-sm" onClick={e => submitForm(e)}>
+    //                     Submit
+    //                 </button>
+    //             </form>
+    //             <div style={{ margin: '2rem' }}>
+    //                 If you are unable to submit this form sucessfully, please email your request to <a href="mailto:course.info@cbtseminary.org">course.info@cbtseminary.org</a>
+    //                 .
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
     return (
-        <div className="container">
+        <div>
+          <button onClick={toggleLanguage}>
+            {language === 'en' ? 'Translate to Spanish' : 'Traducir al Inglés'}
+          </button>
+          {language === 'en' ? (
+                   <div className="container">
             <h2 style={{ color: '#0d6cbf' }}>Password Request Form</h2>
             <div className="form-container">
 
@@ -134,12 +217,6 @@ const PRF = () => {
                             <span className="form-required">
                                 *
                             </span></label>
-                        {/* <select required className="form-control" id="classCode" value={classCode} onChange={(e) => classCodeSelectChanged(e)}>
-                            <option value="none" defaultValue="" disabled="" hidden="">Select Class Code</option>
-                            {titles.map((x, idx) => (
-                                <option key={idx} value={x}>{x}</option>
-                            ))}
-                        </select> */}
                         <Select options={titles.map((x, idx) => ({value:x, label:x}))} onChange={(e) => {classCodeSelectChanged(e.value)}} />
                     </div>
 
@@ -166,8 +243,58 @@ const PRF = () => {
                 </div>
             </div>
         </div>
-    )
-
+          ) : (
+            <div className="container">
+              <h2 style={{ color: '#0d6cbf' }}>Formulario de Solicitud de Contraseña</h2>
+              <div className="form-container">
+                <form id="mainForm">
+                  <div className="form-group w-100">
+                    <label htmlFor="studentFirst">Nombre</label>
+                    <input required type="text" className="form-control" id="studentFirst" name="fname" value={user.fname} onChange={handleChange} />
+                  </div>
+                  <div className="form-group w-100">
+                    <label htmlFor="studentLast">Apellido</label>
+                    <input required type="text" className="form-control" id="studentLast" name="lname" value={user.lname} onChange={handleChange} />
+                  </div>
+                  <div className="form-group w-100">
+                    <label htmlFor="studentEmail">Correo Electrónico del Estudiante</label>
+                    <input required type="email" className="form-control" id="studentEmail" name="email" value={user.email} onChange={handleChange} />
+                    <small>estudiante@ejemplo.com</small>
+                  </div>
+                  <div className="form-group w-100">
+                    <label htmlFor="proctorName">Nombre del Supervisor</label>
+                    <input required type="text" className="form-control" id="proctorName" name="pname" value={user.pname} onChange={handleChange} />
+                  </div>
+                  <div className="form-group w-100">
+                    <label htmlFor="proctorEmail">Correo Electrónico del Supervisor</label>
+                    <input required type="email" className="form-control" id="proctorEmail" name="pemail" value={user.pemail} onChange={handleChange} />
+                    <small>supervisor@ejemplo.com</small>
+                  </div>
+                  <div className="form-group w-100">
+                    <label htmlFor="classCode">Código y Título de la Clase<span className="form-required">*</span></label>
+                    <Select options={titles.map((x, idx) => ({ value: x, label: x }))} onChange={(e) => { classCodeSelectChanged(e.value) }} />
+                  </div>
+                  <div className="form-group w-100">
+                    <label htmlFor="testNumber">Número de Prueba/Examen<span className="form-required">*</span></label>
+                    <select required className="form-control" id="testNumber" value={quizCode} onChange={(e) => selectedTest(e)}>
+                      <option value="none" defaultValue="" disabled="" hidden="">Seleccionar Prueba/Examen</option>
+                      {availTest.map((x, idx) => (
+                        <option key={idx} value={x.testName}>{x.testName}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <button type="submit" className="btn  btn-sm btn-block w-100 shadow-sm" onClick={e => submitForm(e)}>
+                    Enviar
+                  </button>
+                </form>
+                <div style={{ margin: '2rem' }}>
+                  Si no puede enviar este formulario con éxito, envíe su solicitud por correo electrónico a <a href="mailto:course.info@cbtseminary.org">course.info@cbtseminary.org</a>.
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      );
 
 }
 export default PRF
